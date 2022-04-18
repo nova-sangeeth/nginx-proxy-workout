5ce255ffb3199ca9152c4654b45f3becc792cb32
nova-sangeeth <novasangeeth@outlook.com>
2022-04-18 22:36:48 +0530

    Changes to show a html templates

--- fastapi-backend/app.py
+++ fastapi-backend/app.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI
-
+from fastapi.responses import HTMLResponse
 
 app = FastAPI()
 
@@ -8,3 +8,18 @@ app = FastAPI()
 def hello_world():
     message = "Fastapi is working."
     return {"message": message}
+
+# function to render a html template.
+@app.get("/fastapi/html")
+def html_template():
+    return HTMLResponse("""
+    <html>
+        <head>
+            <title>Fastapi-server</title>
+        </head>
+        <body>
+            <h1>Fastapi is server is up.</h1>
+        </body>
+    </html>
+
+    """)
