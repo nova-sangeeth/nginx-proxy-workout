a749a926c53086059fdba1258071ae6c6f4690f8
nova-sangeeth <novasangeeth@outlook.com>
2022-04-18 23:03:06 +0530

    Changes to the express endpoints.

--- express-api/index.js
+++ express-api/index.js
@@ -3,7 +3,20 @@ const app = express();
 const port = 3000;
 const host = "0.0.0.0";
 
-app.get("/express-api", (req, res) => res.send("Express API is working..."));
+app.get(
+  "/express-api", (req, res) => 
+  res.send(
+    `<html>
+      <body>
+        <center>
+          <h1>Express App works on port ${port}</h1>
+          <h1>Request host name: ${req.hostname}</h1>
+        </center>
+      </body>
+    </html>`
+    )
+  );
+
 
 app.listen(port, host, () =>
   console.log(`Example app listening at http://${host}:${port}`)
