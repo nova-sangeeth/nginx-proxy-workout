a97d061805ec5bda77c1ed89fd313c7eff30fea8
nova-sangeeth <novasangeeth@outlook.com>
2022-07-03 22:26:35 +0530

    fix issues with express.

--- express-api/index.js
+++ express-api/index.js
@@ -3,20 +3,19 @@ const app = express();
 const port = 3000;
 const host = "0.0.0.0";
 
-app.get(
-  "/express-api", (req, res) => 
+app.get("/", (req, res) =>
   res.send(
     `<html>
       <body>
         <center>
-          <h1>Express App works on port ${port}</h1>
-          <h1>Request host name: ${req.hostname}</h1>
+          <h1>Express App works on host and port ${host}:${port}</h1>
+          <h1>Express App works on request with hostname ${req.hostname}</h1>
+          <h1>Express App works on request with headers ${req.headers}</h1>
         </center>
       </body>
     </html>`
-    )
-  );
-
+  )
+);
 
 app.listen(port, host, () =>
   console.log(`Example app listening at http://${host}:${port}`)
