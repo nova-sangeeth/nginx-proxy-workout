4485acf808aed1d2ca9087fe6a7a12bdc0cb000f
nova-sangeeth <novasangeeth@outlook.com>
2022-08-26 00:44:29 +0530

    changes to add rsuite

--- react-vite/src/App.tsx
+++ react-vite/src/App.tsx
@@ -1,32 +1,28 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import './App.css'
+import { Button, IconButton, ButtonGroup, ButtonToolbar, Navbar } from 'rsuite';
+import NavBar from 'rsuite';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const instance = (
+    <ButtonToolbar>
+      <Button appearance="default">Default</Button>
+    </ButtonToolbar>
+  );
+
+
+  const navbarInstance = (
+    <Navbar>
+    <Navbar.Brand>
+      Hello
+    </Navbar.Brand>
+    </Navbar>
+  )
 
   return (
     <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Changes done.
-      </p>
+      {instance}
     </div>
   )
 }
