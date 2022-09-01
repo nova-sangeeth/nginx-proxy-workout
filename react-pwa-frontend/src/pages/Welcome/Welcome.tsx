6d1840af605bc34222a2ad693b73a49702b4ae37
nova-sangeeth <novasangeeth@outlook.com>
2022-09-02 01:14:05 +0530

    Changes to the welcome pages

--- react-pwa-frontend/src/pages/Welcome/Welcome.tsx
+++ react-pwa-frontend/src/pages/Welcome/Welcome.tsx
@@ -5,20 +5,10 @@ import Meta from "@/components/Meta";
 import { FullSizeCenteredFlexBox } from "@/components/styled";
 import useOrientation from "@/hooks/useOrientation";
 
-import muiLogo from "./logos/mui.svg";
-import pwaLogo from "./logos/pwa.svg";
-import reactLogo from "./logos/react_ed.svg";
-import recoilLogo from "./logos/recoil.svg";
-import rrLogo from "./logos/rr.svg";
-import tsLogo from "./logos/ts.svg";
-import viteLogo from "./logos/vite.svg";
 import { Image } from "./styled";
 
 function Welcome() {
   const isPortrait = useOrientation();
-
-  const width = isPortrait ? "40%" : "30%";
-  const height = isPortrait ? "30%" : "40%";
 
   return (
     <>
