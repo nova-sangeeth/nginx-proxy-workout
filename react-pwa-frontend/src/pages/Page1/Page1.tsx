f82638f010aa80addd625d3eea5359b0c85011a5
nova-sangeeth <novasangeeth@outlook.com>
2022-09-10 01:16:12 +0530

    Changes to add the new tech stack.

--- react-pwa-frontend/src/pages/Page1/Page1.tsx
+++ react-pwa-frontend/src/pages/Page1/Page1.tsx
@@ -1,15 +1,52 @@
-import Typography from '@mui/material/Typography';
+import Chip from "@mui/material/Chip";
+import Container from "@mui/material/Container";
+import Grid from "@mui/material/Grid";
+import Paper from "@mui/material/Paper";
+import Stack from "@mui/material/Stack";
 
-import Meta from '@/components/Meta';
-import { FullSizeCenteredFlexBox } from '@/components/styled';
+import Meta from "@/components/Meta";
 
 function Page1() {
   return (
     <>
       <Meta title="Tech Stack" />
-      <FullSizeCenteredFlexBox>
-        <Typography variant="h3">Tech Stack</Typography>
-      </FullSizeCenteredFlexBox>
+
+      <Container disableGutters={true}>
+        <h1>Tech Stack</h1>
+        <h4>Languages</h4>
+        <Stack direction="row" spacing={1}>
+          <Chip label="Python" />
+          <Chip label="JavaScript" />
+          <Chip label="TypeScript" />
+          <Chip label="SQL" />
+          <Chip label="Bash" />
+        </Stack>
+        <h4>Frameworks</h4>
+        <Stack direction="row" spacing={1}>
+          <Chip label="FastAPI" />
+          <Chip label="Django" />
+          <Chip label="Flask" />
+          <Chip label="Express" />
+          <Chip label="React" />
+          <Chip label="Vue" />
+        </Stack>
+        <h4>Containerization Technologies</h4>
+        <Stack direction="row" spacing={1}>
+          <Chip label="Docker" />
+        </Stack>
+        <h4>Versioning Systems</h4>
+        <Stack direction="row" spacing={1}>
+          <Chip label="Git" />
+          <Chip label="GitHub" />
+          <Chip label="GitLab" />
+        </Stack>
+        <h4>Cloud Providers</h4>
+        <Stack direction="row" spacing={1}>
+          <Chip label="Azure" />
+          <Chip label="AWS" />
+          <Chip label="DigitalOcean" />
+        </Stack>
+      </Container>
     </>
   );
 }
