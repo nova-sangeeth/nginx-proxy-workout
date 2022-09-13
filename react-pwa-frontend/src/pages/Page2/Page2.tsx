6f01c7e1cfd40c8f7290674f1e268f0bb180295b
nova-sangeeth <novasangeeth@outlook.com>
2022-09-13 23:59:43 +0530

    work on adding stepper

--- react-pwa-frontend/src/pages/Page2/Page2.tsx
+++ react-pwa-frontend/src/pages/Page2/Page2.tsx
@@ -1,14 +1,37 @@
-import Typography from '@mui/material/Typography';
+import Step from "@mui/material/Step";
+import StepContent from "@mui/material/StepContent";
+import StepLabel from "@mui/material/StepLabel";
+import Stepper from "@mui/material/Stepper";
+import Typography from "@mui/material/Typography";
 
-import Meta from '@/components/Meta';
-import { FullSizeCenteredFlexBox } from '@/components/styled';
+import Meta from "@/components/Meta";
+import { FullSizeCenteredFlexBox } from "@/components/styled";
 
 function Page2() {
   return (
     <>
       <Meta title="Projects" />
       <FullSizeCenteredFlexBox>
-        <Typography variant="h3">Projects</Typography>
+        <Typography variant="h5">Projects</Typography>
+
+        <Stepper  orientation="vertical">
+          <Step>
+            <StepLabel>Step 1</StepLabel>
+            <StepContent></StepContent>
+          </Step>
+          <Step>
+            <StepLabel>Step 2</StepLabel>
+            <StepContent></StepContent>
+          </Step>
+          <Step>
+            <StepLabel>Step 3</StepLabel>
+            <StepContent></StepContent>
+          </Step>
+          <Step>
+            <StepLabel>Step 4</StepLabel>
+            <StepContent></StepContent>
+          </Step>
+        </Stepper>
       </FullSizeCenteredFlexBox>
     </>
   );
