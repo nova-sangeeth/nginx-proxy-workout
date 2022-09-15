8af5f776921c10fe6eb54eca3131dcf904d60387
nova-sangeeth <novasangeeth@outlook.com>
2022-09-15 23:44:55 +0530

    work on cards for stats.

--- react-pwa-frontend/src/pages/Stats/Stats.tsx
+++ react-pwa-frontend/src/pages/Stats/Stats.tsx
@@ -1,43 +1,25 @@
+import { Card, CardActions, CardContent } from "@mui/material";
 import { Typography } from "@mui/material";
-import Grid from "@mui/material/Grid";
-import Box from "@mui/material/Grid";
-import Paper from "@mui/material/Paper";
+import { Theme, createStyles, makeStyles } from "@mui/material";
 
 import { FullSizeCenteredFlexBox } from "@/components/styled";
 
 const Stats = () => {
   return (
     <>
-      <Grid container spacing={2} columns={16}>
-        <Grid item xs={8}>
-          <Paper>
-            <Typography>Number of Hits today</Typography>
-          </Paper>
-        </Grid>
-        <Grid item xs={8}>
-          <Paper>
-            <Typography>Total Number of Hits</Typography>
-          </Paper>
-        </Grid>
-      </Grid>
-      <Grid container spacing={2} columns={16}>
-        <Grid item xs={8}>
-          <Paper>
-            <Typography>Version number</Typography>
-          </Paper>
-        </Grid>
-        <Grid item xs={8}>
-          <Typography>2</Typography>
-        </Grid>
-      </Grid>
-      <Grid container spacing={2} columns={16}>
-        <Grid item xs={8}>
-          <Typography>1</Typography>
-        </Grid>
-        <Grid item xs={8}>
-          <Typography>2</Typography>
-        </Grid>
-      </Grid>
+      <FullSizeCenteredFlexBox>
+        <Card sx={{ minWidth: 375 }}>
+          <CardContent>
+            <Typography>Stat Card 1</Typography>
+            <Typography>Stat Content</Typography>
+          </CardContent>
+        </Card>
+        <Card sx={{ minWidth: 375 }}>
+          <CardContent>
+            <Typography>Stat Content</Typography>
+          </CardContent>
+        </Card>
+      </FullSizeCenteredFlexBox>
     </>
   );
 };
