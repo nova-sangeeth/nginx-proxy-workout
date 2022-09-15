93bfbbd549bf46a97380644528cc4c5e73e1622b
nova-sangeeth <novasangeeth@outlook.com>
2022-09-16 00:01:21 +0530

    add states and hooks for backend reponse stuff.

--- react-pwa-frontend/src/pages/Stats/Stats.tsx
+++ react-pwa-frontend/src/pages/Stats/Stats.tsx
@@ -1,22 +1,39 @@
-import { Card, CardActions, CardContent } from "@mui/material";
+import { useEffect, useState } from "react";
+
+import { Card, CardContent } from "@mui/material";
 import { Typography } from "@mui/material";
-import { Theme, createStyles, makeStyles } from "@mui/material";
 
 import { FullSizeCenteredFlexBox } from "@/components/styled";
 
 const Stats = () => {
+  const [statItem1, setStatItem1] = useState<string>("");
+  const [statItem2, setStatItem2] = useState<string>("");
+
+  const fetchStats = () => {
+    const temp_values_1 = "stat  1";
+    const temp_values_2 = "stat state 2";
+
+    setStatItem1(temp_values_1);
+    setStatItem2(temp_values_2);
+  };
+
+  useEffect(() => {
+    fetchStats();
+  }, [statItem1]);
+
   return (
     <>
       <FullSizeCenteredFlexBox>
         <Card sx={{ minWidth: 375 }}>
           <CardContent>
             <Typography>Stat Card 1</Typography>
-            <Typography>Stat Content</Typography>
+            <Typography>{statItem1}</Typography>
           </CardContent>
         </Card>
-        <Card sx={{ minWidth: 375 }}>
+        <Card sx={{ minWidth: 375, marginLeft: 3 }}>
           <CardContent>
-            <Typography>Stat Content</Typography>
+            <Typography>Stat Card 2</Typography>
+            <Typography>{statItem2}</Typography>
           </CardContent>
         </Card>
       </FullSizeCenteredFlexBox>
