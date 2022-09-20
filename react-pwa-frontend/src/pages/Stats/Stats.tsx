3c09323b8994c26caf1d21268875c9778cc7f741
nova-sangeeth <novasangeeth@outlook.com>
2022-09-20 23:55:37 +0530

    changes to add recoil state with it's atom option in it.

--- react-pwa-frontend/src/pages/Stats/Stats.tsx
+++ react-pwa-frontend/src/pages/Stats/Stats.tsx
@@ -1,25 +1,33 @@
 import { useEffect, useState } from "react";
+import { atom, useRecoilState } from "recoil";
 
 import { Card, CardContent } from "@mui/material";
 import { Typography } from "@mui/material";
 
 import { FullSizeCenteredFlexBox } from "@/components/styled";
 
+// create a recoil state.
+const StatsState = atom<string>({
+  key: "stat-item-1",
+  default: "",
+});
+
 const Stats = () => {
-  const [statItem1, setStatItem1] = useState<string>("");
-  const [statItem2, setStatItem2] = useState<string>("");
+  // const [statItem1, setStatItem1] = useState<string>("");
+  // const [statItem2, setStatItem2] = useState<string>("");
+
+  // use recoil state management.
+  const [statItem1, setStatItem1] = useRecoilState(StatsState);
 
   const fetchStats = () => {
-    const temp_values_1 = "stat  1";
-    const temp_values_2 = "stat state 2";
+    const temp_values_1 = "updated state";
 
     setStatItem1(temp_values_1);
-    setStatItem2(temp_values_2);
   };
 
   useEffect(() => {
     fetchStats();
-  }, [statItem1]);
+  });
 
   return (
     <>
@@ -33,7 +41,7 @@ const Stats = () => {
         <Card sx={{ minWidth: 375, marginLeft: 3 }}>
           <CardContent>
             <Typography>Stat Card 2</Typography>
-            <Typography>{statItem2}</Typography>
+            {/* <Typography>{statItem2}</Typography> */}
           </CardContent>
         </Card>
       </FullSizeCenteredFlexBox>
