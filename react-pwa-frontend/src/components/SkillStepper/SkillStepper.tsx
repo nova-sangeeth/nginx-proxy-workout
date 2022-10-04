7bf3d59be19aa55caaf0501b158ddbe9103ea200
nova-sangeeth <novasangeeth@outlook.com>
2022-10-04 23:17:26 +0530

    Add component for skill stepper.

---
+++ react-pwa-frontend/src/components/SkillStepper/SkillStepper.tsx
@@ -0,0 +1,68 @@
+// import * as React from "react";
+import { useState } from "react";
+
+import Box from "@mui/material/Box";
+import Button from "@mui/material/Button";
+import Paper from "@mui/material/Paper";
+import Step from "@mui/material/Step";
+import StepContent from "@mui/material/StepContent";
+import StepLabel from "@mui/material/StepLabel";
+import Stepper from "@mui/material/Stepper";
+import Typography from "@mui/material/Typography";
+
+import projectContent from "./content";
+
+function SkillStepper() {
+  const [activeStep, setActiveStep] = useState(0);
+
+  const handleNext = () => {
+    setActiveStep((prevActiveStep) => prevActiveStep + 1);
+  };
+
+  const handleBack = () => {
+    setActiveStep((prevActiveStep) => prevActiveStep - 1);
+  };
+
+  const handleReset = () => {
+    setActiveStep(0);
+  };
+
+  return (
+    <Box sx={{ maxWidth: 400 }}>
+      <Stepper activeStep={activeStep} orientation="vertical">
+        {projectContent.map((step, index) => (
+          <Step key={step.label}>
+            <StepLabel
+              optional={index === 2 ? <Typography variant="caption">Last step</Typography> : null}
+            >
+              {step.label}
+            </StepLabel>
+            <StepContent>
+              <Typography>{step.description}</Typography>
+              <Box sx={{ mb: 2 }}>
+                <div>
+                  <Button variant="contained" onClick={handleNext} sx={{ mt: 1, mr: 1 }}>
+                    {index === projectContent.length - 1 ? "Finish" : "Next"}
+                  </Button>
+                  <Button disabled={index === 0} onClick={handleBack} sx={{ mt: 1, mr: 1 }}>
+                    Back
+                  </Button>
+                </div>
+              </Box>
+            </StepContent>
+          </Step>
+        ))}
+      </Stepper>
+      {activeStep === projectContent.length && (
+        <Paper square elevation={0} sx={{ p: 3 }}>
+          <Typography>All projectContent completed - you&apos;re finished</Typography>
+          <Button onClick={handleReset} sx={{ mt: 1, mr: 1 }}>
+            Reset
+          </Button>
+        </Paper>
+      )}
+    </Box>
+  );
+}
+
+export default SkillStepper;
