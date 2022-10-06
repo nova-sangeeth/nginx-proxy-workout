4c5a3b303e41c905f86178f82ee4e3ab25c53dc4
nova-sangeeth <novasangeeth@outlook.com>
2022-10-06 11:28:44 +0530

    move the content to a separate file.

---
+++ react-pwa-frontend/src/components/SkillStepper/content.ts
@@ -0,0 +1,18 @@
+const projectContent = [
+  {
+    label: "Drix URL",
+    description: `Simple Application to shorten URLs and keep the contents of it safe.`,
+  },
+  {
+    label: "Flask Nginx Proxxy",
+    description:
+      "A project to test out servers and show maintainance window pages during deployment.",
+  },
+  {
+    label: "QnA App",
+    description: `QnA is a reddit clone which is used as a small scale amazon comments section.`,
+  },
+];
+
+
+export default projectContent
