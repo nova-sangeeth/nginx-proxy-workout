676708d37a4c4cd940ba4875933f14151e127ab3
nova-sangeeth <novasangeeth@outlook.com>
2022-10-10 16:28:22 +0530

    Add new content to the  about me page.

--- react-pwa-frontend/src/pages/Page3/Page3.tsx
+++ react-pwa-frontend/src/pages/Page3/Page3.tsx
@@ -1,14 +1,26 @@
-import Typography from '@mui/material/Typography';
+import Box from "@mui/material/Box";
+import Card from "@mui/material/Card";
+import CardContent from "@mui/material/CardContent";
+import Typography from "@mui/material/Typography";
 
-import Meta from '@/components/Meta';
-import { FullSizeCenteredFlexBox } from '@/components/styled';
+import Meta from "@/components/Meta";
+import { FullSizeCenteredFlexBox } from "@/components/styled";
 
 function Page3() {
   return (
     <>
       <Meta title="About Me" />
       <FullSizeCenteredFlexBox>
-        <Typography variant="h3">About Me</Typography>
+        <Card sx={{ minWidth: 275 }}>
+          <CardContent>
+            <Typography>About me Content</Typography>
+            <Typography>Hello There</Typography>
+            <Typography>
+              I am a full Stack developer who is passionate about technology and building ideal
+              appications to solve real world problems.
+            </Typography>
+          </CardContent>
+        </Card>
       </FullSizeCenteredFlexBox>
     </>
   );
