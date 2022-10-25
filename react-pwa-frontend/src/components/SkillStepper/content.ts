910a249cc1fc848daf7925eecf48cba72c64c9d7
nova-sangeeth <novasangeeth@outlook.com>
2022-10-25 23:04:26 +0530

    update the content for the vertical stepper.

--- react-pwa-frontend/src/components/SkillStepper/content.ts
+++ react-pwa-frontend/src/components/SkillStepper/content.ts
@@ -2,19 +2,23 @@ const projectContent = [
   {
     label: "Drix URL",
     description: `Simple Application to shorten URLs and keep the contents of it safe.`,
+    project_source_url: "https://github.com/nova-sangeeth/drix-url",
   },
   {
     label: "Flask Nginx Proxxy",
     description:
       "A project to test out servers and show maintainance window pages during deployment.",
+    project_source_url: "TBA",
   },
   {
     label: "QnA App",
     description: `QnA is a reddit clone which is used as a small scale amazon comments section.`,
+    project_source_url: "TBA",
   },
   {
     label: "Scrapy Scraper",
     description: `Amazon review scraper which is used scrape the user details and rankings along with the reviews of a give product.`,
+    project_source_url: "TBA",
   },
 ];
 
