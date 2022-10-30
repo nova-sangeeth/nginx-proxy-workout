39b2e1dd785e040b634e029147b402787cf41dff
nova-sangeeth <novasangeeth@outlook.com>
2022-10-31 00:01:59 +0530

    Changes to add links to the projects that are mentioned.

--- react-pwa-frontend/src/components/SkillStepper/content.ts
+++ react-pwa-frontend/src/components/SkillStepper/content.ts
@@ -5,20 +5,20 @@ const projectContent = [
     project_source_url: "https://github.com/nova-sangeeth/drix-url",
   },
   {
-    label: "Flask Nginx Proxxy",
+    label: "Flask Nginx Proxy",
     description:
       "A project to test out servers and show maintainance window pages during deployment.",
-    project_source_url: "TBA",
+    project_source_url: "https://github.com/nova-sangeeth/nginx-flask-application",
   },
   {
     label: "QnA App",
     description: `QnA is a reddit clone which is used as a small scale amazon comments section.`,
-    project_source_url: "TBA",
+    project_source_url: "https://github.com/nova-sangeeth/QnA-application",
   },
   {
     label: "Scrapy Scraper",
     description: `Amazon review scraper which is used scrape the user details and rankings along with the reviews of a give product.`,
-    project_source_url: "TBA",
+    project_source_url: "https://github.com/nova-sangeeth/scraper-scrapy",
   },
 ];
 
