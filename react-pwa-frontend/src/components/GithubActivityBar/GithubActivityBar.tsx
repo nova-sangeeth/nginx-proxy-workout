520ed7fc0543b4f8774468dc65f966927a6eb26e
nova-sangeeth <novasangeeth@outlook.com>
2022-12-05 23:05:31 +0530

    Changes to the github activity bar component.

--- react-pwa-frontend/src/components/GithubActivityBar/GithubActivityBar.tsx
+++ react-pwa-frontend/src/components/GithubActivityBar/GithubActivityBar.tsx
@@ -1,5 +1,7 @@
 import GitHubCalendar from "react-github-calendar";
 
+import Container from "@mui/material/Container";
+
 import Meta from "@/components/Meta";
 
 import gitHubUsername from "./meta";
@@ -7,10 +9,11 @@ import gitHubUsername from "./meta";
 function GithubActivityBar() {
   console.log("Github Activity Bar.");
   return (
-    <>
+    <Container disableGutters={true}>
       <Meta title="Github Activity Bar" />
+      <h1>My Coding Activity</h1>
       <GitHubCalendar username={gitHubUsername} />
-    </>
+    </Container>
   );
 }
 
